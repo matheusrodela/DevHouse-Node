53bef643402f9064ae68e4dd856a27d28ef5f372
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-18 20:03:49 -0300

    eslint and editor config

--- src/app.js
+++ src/app.js
@@ -4,36 +4,34 @@ import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
-class App{
-    constructor(app){
-        this.server = express(); //this faz referência ao App
+class App {
+  constructor() {
+    this.server = express(); // this faz referência ao App
 
-        mongoose.connect('mongodb+srv://MrDev:devhouse@devhouse.unbli7h.mongodb.net/?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useunifiedtopology: true,
-        })
+    mongoose.connect('mongodb+srv://MrDev:devhouse@devhouse.unbli7h.mongodb.net/?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useunifiedtopology: true,
+    });
 
-        //chamar métodos. Se não eles não são executados
-        this.middlewares();
-        this.routes();
-    }
+    // chamar métodos. Se não eles não são executados
+    this.middlewares();
+    this.routes();
+  }
 
+  middlewares() {
+    this.server.use(cors());
 
-    middlewares(){
-        this.server.use(cors());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'uploads')),
+    );
 
-        this.server.use(
-            '/files',
-            express.static(path.resolve(__dirname, '..', 'uploads'))
-        );
+    this.server.use(express.json()); // declara que o servidor usa json
+  }
 
-        this.server.use(express.json()); //declara que o servidor usa json
-    }
-
-    routes(){
-        this.server.use(routes);
-    }
-
+  routes() {
+    this.server.use(routes);
+  }
 }
 
-export default new App().server
+export default new App().server;
