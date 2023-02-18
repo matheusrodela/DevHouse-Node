9f1d965fcacb87d68bf1fa30744ebb422e7b57cb
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-18 19:09:44 -0300

    validando email para logar

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,10 +1,19 @@
 import User  from '../models/User';
     //importa User do model Users e permite que a aplicação crie, edite e apague usuários.
+import * as Yup from 'yup';
 
 class SessionController{
     async store(req, res){
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        });
+
         //busca o email do body
         const email = req.body.email;
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: 'Digite um email válido para logar.' })
+        }
         
         //usa o email buscado para criar usuário no BD
         //let user = await User.create({ email: email });
