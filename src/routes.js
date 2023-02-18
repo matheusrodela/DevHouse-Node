b0c4f420c4dbf15ff8b52ddb35b2c12e6aa6a2f7
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-02-18 14:49:17 -0300

    editando e excluindo casas

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,15 @@ import uploadConfig from './config/upload'
 import SessionControllers from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 
+
 const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionControllers.store);
 
 routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
+routes.delete('/houses', HouseController.destroy);
 
 export default routes;
